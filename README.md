77c7f9ff00e64cd273eb70e62175c960149dd822
alfarishi-git <62209112+ralfarishi@users.noreply.github.com>
2023-09-28 21:45:13 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,69 @@
+# What is this
+
+CredX is a program to store personal account credential in command line. The stored data is encrypted using Fernet so it is not easily accessed by irresponsible people. The program also has a login process to increase the security.
+
+# How To Use
+
+## Install Modules
+
+- Python v3.8.10
+- Pip v23.2.1
+
+1. Clone this repo into your local computer. Then move inside to the cloned folder.
+
+```bash
+git clone https://github.com/ralfarishi/CredX.git
+cd /CredX
+```
+
+2. Install all modules using pip.
+
+```bash
+pip install cryptography bcrypt pyfiglet termcolor
+```
+
+3. Wait until the installation complete.
+
+## Generate Key
+
+1. Open the `generate_key.py` file. There are 2 method inside the file `hashed_password()` & `encryption_key()`
+2. In `hashed_password()` method, change the `password` variable value to your password string.
+
+```py
+# example
+password = "secretPassword123"
+```
+
+3. Run the `generate_key.py` file.
+
+```bash
+python generate_key.py
+```
+
+4. You will see 2 outputs like the example below. The first line is your password key & the second line is the encryption key.
+
+```bash
+b'$2b$12$.xHvyKPhN1FMo/lh2.UWWeypwUwRd2xAlWuLEsCAYnkcdQfJhCeEG'
+JMhBx7NRjrnyGZN0jKzuyGCSV9Y9gBKy-qbghcaVpiA=
+```
+
+## Key Configuration
+
+1. Open the `main.py` file and find the `login()` method.
+2. Copy the keys you generate earlier to the `hashed_password` & `encryption_key` variables.
+
+```python
+# example
+hashed_password = b"$2b$12$.xHvyKPhN1FMo/lh2.UWWeypwUwRd2xAlWuLEsCAYnkcdQfJhCeEG"
+encryption_key = b"JMhBx7NRjrnyGZN0jKzuyGCSV9Y9gBKy-qbghcaVpiA="
+```
+
+3. Now you can run & use the program.
+
+## Optional
+
+You can change the login username in the `login()` method. Find the `if input_username == "Potus"` code around line 88. Then change the `"Potus"` string to your username.
+
+---
+
+\*note: I'm working on to make the executable for this program, so if you're not familiar with python you can use it by running the `.exe` file.
---
+++ main.py
@@ -0,0 +1,182 @@
+import getpass, bcrypt, time, pickle
+from cryptography.fernet import Fernet
+from pyfiglet import Figlet
+from termcolor import colored
+
+
+class AccountManager:
+    def __init__(self, encryption_key):
+        self.accounts = []
+        self.encryption_key = encryption_key
+
+    def add_account(self, account_type, username, password):
+        account = {
+            "Account Type": account_type,
+            "Username": username,
+            "Password": password,
+        }
+        self.accounts.append(account)
+        print(f"\33[32mAkun berhasil ditambahkan.\33[0m")
+
+    def update_account(
+        self, account_index, new_account_type, new_username, new_password
+    ):
+        if 0 <= account_index < len(self.accounts):
+            account = self.accounts[account_index]
+            if new_account_type:
+                account["Account Type"] = new_account_type
+            if new_username:
+                account["Username"] = new_username
+            if new_password:
+                account["Password"] = new_password
+            print(f"\33[34mData akun berhasil diperbarui.\33[0m")
+        else:
+            print("Nomor akun tidak valid!")
+
+    def delete_account(self, account_index):
+        if account_index >= 0 and account_index < len(self.accounts):
+            deleted_account = self.accounts.pop(account_index)
+            print(
+                f"Akun \x1B[3m\033[31m{deleted_account['Account Type']}\033[0m\x1B[0m berhasil dihapus."
+            )
+        else:
+            print("Nomor akun tidak valid.")
+
+    def view_accounts(self):
+        if not self.accounts:
+            print("Tidak ada akun yang tersedia.")
+        else:
+            print("\nDaftar Akun:")
+            for i, account in enumerate(self.accounts, start=1):
+                print(f"{i}. {account['Account Type']}")
+
+    def get_account(self, account_index):
+        if 0 <= account_index < len(self.accounts):
+            return self.accounts[account_index]
+        else:
+            return None
+
+    def save_to_file(self, filename):
+        encrypted_data = Fernet(self.encryption_key).encrypt(
+            pickle.dumps(self.accounts)
+        )
+        with open(filename, "wb") as file:
+            file.write(encrypted_data)
+            print(f"Data berhasil disimpan ke {filename}")
+
+    def load_from_file(self, filename):
+        try:
+            with open(filename, "rb") as file:
+                encrypted_data = file.read()
+                decrypted_data = Fernet(self.encryption_key).decrypt(encrypted_data)
+                self.accounts = pickle.loads(decrypted_data)
+                print(f"Data berhasil dimuat dari {filename}")
+        except (FileNotFoundError, pickle.UnpicklingError):
+            print(f"File {filename} tidak ditemukan atau data tidak valid.")
+
+
+def login():
+    hashed_password = b"your_hashed_password"
+    encryption_key = b"your_encryption_key"
+
+    fig = Figlet(font="slant")
+    print(colored(fig.renderText("CredX Login"), "blue", attrs=["bold", "blink"]))
+
+    input_username = input("Username: ")
+    input_password = getpass.getpass("Password: ")
+
+    if input_username == "Potus" and bcrypt.checkpw(
+        input_password.encode("utf-8"), hashed_password
+    ):
+        print("Loading...")
+        time.sleep(2.5)
+
+        fig = Figlet(font="slant")
+        print(
+            colored(fig.renderText("Hello, Potus!"), "green", attrs=["bold", "blink"])
+        )
+
+        account_manager = AccountManager(encryption_key)
+        main(account_manager)
+    else:
+        print("Credentials tidak ditemukan!")
+
+
+def main(account_manager):
+    filename = "your-filename.pkl"
+    fig = Figlet(font="slant")
+
+    try:
+        account_manager.load_from_file(filename)
+    except EOFError:
+        print("File data kosong atau tidak valid.")
+
+    while True:
+        print("\nMenu Utama:")
+        print("1. Tambah Akun")
+        print("2. Update Akun")
+        print("3. Lihat Akun")
+        print("4. Hapus Akun")
+        print("5. Simpan dan Keluar")
+        choice = input("Pilih menu (1/2/3/4/5): ")
+
+        if choice == "1":
+            account_type = input("\nMasukkan jenis akun: ")
+            username = input("Masukkan username: ")
+            password = input("Masukkan password: ")
+            account_manager.add_account(account_type, username, password)
+        elif choice == "2":
+            account_manager.view_accounts()
+            if account_manager.accounts:
+                account_index = int(input("Pilih nomor akun yang akan di-update: ")) - 1
+                selected_account = account_manager.get_account(account_index)
+                if selected_account:
+                    new_account_type = input(
+                        "Masukkan jenis akun (kosongkan jika tidak ingin mengubah) : "
+                    )
+                    new_username = input(
+                        "Masukkan username (kosongkan jika tidak ingin mengubah) : "
+                    )
+                    new_password = input(
+                        "Masukkan password (kosongkan jika tidak ingin mengubah) : "
+                    )
+                    account_manager.update_account(
+                        account_index, new_account_type, new_username, new_password
+                    )
+                else:
+                    print("Nomor akun tidak valid.")
+        elif choice == "3":
+            account_manager.view_accounts()
+            if account_manager.accounts:
+                account_index = int(input("Pilih nomor akun yang ingin dilihat: ")) - 1
+                selected_account = account_manager.get_account(account_index)
+                if selected_account:
+                    print(
+                        colored(
+                            fig.renderText(selected_account["Account Type"]),
+                            "red",
+                            attrs=["bold"],
+                        )
+                    )
+                    print(f"Username: {selected_account['Username']}")
+                    print(f"Password: {selected_account['Password']}")
+                else:
+                    print("Nomor akun tidak valid.")
+        elif choice == "4":
+            account_manager.view_accounts()
+            if account_manager.accounts:
+                account_index = int(input("Pilih nomor akun yang akan dihapus: ")) - 1
+                account_to_delete = account_manager.get_account(account_index)
+                if account_to_delete:
+                    account_manager.delete_account(account_index)
+                else:
+                    print("Nomor akun tidak valid.")
+        elif choice == "5":
+            account_manager.save_to_file(filename)
+            break
+        else:
+            print("Pilihan tidak valid. Silakan coba lagi.")
+
+
+if __name__ == "__main__":
+    login()
